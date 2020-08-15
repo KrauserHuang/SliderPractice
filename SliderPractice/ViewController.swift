7c646670eb356d550d0cb36540c5dcfffcac29c2
krauserhuang <crescentdence@gmail.com>
2020-08-15 15:53:11 +0800

    Initial Commit

---
+++ SliderPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderPractice
+//
+//  Created by Tai Chin Huang on 2020/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
